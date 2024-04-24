6ba3e6038c0ca9539dccad1af2ff1868c73f0116
John192601 <163221351+John192601@users.noreply.github.com>
2024-04-24 18:54:50 -0500

    DOM

--- egg_js/example/stringIncludes.js
+++
@@ -1,8 +0,0 @@
-function stringIncludes(substring, string) {
-    return string.includes(substring);
-  }
-  
-  // Ejemplos de uso
-  console.log(stringIncludes("hello", "hello world")); // Devuelve true
-  console.log(stringIncludes("world", "hello world")); // Devuelve true
-  console.log(stringIncludes("foo", "hello world")); // Devuelve false
